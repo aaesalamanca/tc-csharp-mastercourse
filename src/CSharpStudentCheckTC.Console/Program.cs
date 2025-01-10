82064857047eb2988c7cc77a6ac76a24a5946490
aaesalamanca <aaesalamanca@gmail.com>
2025-01-10 15:54:32 +0100

    feat: Ask user info (first name & age)

--- src/CSharpStudentCheckTC.Console/Program.cs
+++ src/CSharpStudentCheckTC.Console/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string AskUserForInfo(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine() ?? string.Empty;
+}
+
+string AskUserForFirstName()
+{
+    do
+    {
+        var firstName = AskUserForInfo("What is your first name: ");
+
+        if (
+            string.IsNullOrWhiteSpace(firstName)
+            || int.TryParse(firstName, out int intFromName)
+            || double.TryParse(firstName, out double doubleFromName)
+        )
+        {
+            Console.WriteLine("Invalid first name.");
+            continue;
+        }
+
+        return firstName;
+    } while (true);
+}
+
+int AskUserForAge()
+{
+    do
+    {
+        var textAge = AskUserForInfo("What is your age: ");
+
+        if (
+            string.IsNullOrWhiteSpace(textAge)
+            || !int.TryParse(textAge, out int intFromName)
+            || intFromName < 1
+            || !double.TryParse(textAge, out double doubleFromName)
+        )
+        {
+            Console.WriteLine("Invalid age.");
+            continue;
+        }
+
+        return int.Parse(textAge);
+    } while (true);
+}
+
+var firstName = AskUserForFirstName();
+var age = AskUserForAge();
