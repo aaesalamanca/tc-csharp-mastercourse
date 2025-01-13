090b1a79b1b2e517006531364d53326818b1aae9
Alejandro Estornell <aaesalamanca@gmail.com>
2025-01-13 16:35:58 +0100

    feat: Ask user info & show message (#1)
    * docs: Add README file
    
    * feat: Add console project
    
    * docs: Fix path to execute the console project
    
    * style: Add CSharpier for formatting
    
    * feat: Ask user info (first name & age)
    
    * feat: Show app info on startup
    
    * feat: Show the output message to the console
    
    * build: Enable treating warnings as errors

---
+++ README.md
@@ -0,0 +1,11 @@
+# CSharpStudentCheckTC
+
+Esta aplicación de consola está basada en el primer mini proyecto propuesto por Tim Corey en su curso: [C# Mastercourse](https://www.iamtimcorey.com/courses/csharp-mastercourse/).
+
+La aplicación debe solicitar al usuario su nombre (no apellido) y su edad. Posteriormente, si su nombre es Bob o Sue, mostrará un mensaje de bienvenida personalizado para profesores. Por el contrario, mostrará un mensaje genérico para estudiantes. Asimismo, si el usuario introduce una edad inferior a 21 años, se le indicará cuántos años debe esperar para comenzar la clase.
+
+Para ejecutar la aplicación tras clonar el repositorio, sitúese en la raíz del directorio y ejecutar:
+
+```zsh
+dotnet run --project src/CSharpStudentCheckTC.Console/
+```
---
+++ src/CSharpStudentCheckTC.Console/Program.cs
@@ -0,0 +1,75 @@
+﻿ShowAppInfo();
+var firstName = AskUserForFirstName();
+var age = AskUserForAge();
+
+var outputMessage =
+    "Hello, "
+    + firstName.ToLower() switch
+    {
+        "bob" or "sue" => $"Professor {firstName}",
+        _ => firstName,
+    }
+    + ". ";
+
+outputMessage += age switch
+{
+    >= 21 => "Welcome to the class.",
+    _ => $"You have to wait {21 - age} years to join the class.",
+};
+
+Console.WriteLine(outputMessage);
+
+void ShowAppInfo()
+{
+    Console.WriteLine("####################################");
+    Console.WriteLine("####### CSharpStudentCheckTC #######");
+    Console.WriteLine("####################################");
+    Console.WriteLine();
+}
+
+string AskUserForInfo(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine() ?? string.Empty;
+}
+
+string AskUserForFirstName()
+{
+    do
+    {
+        var firstName = AskUserForInfo("What is your first name: ");
+
+        if (
+            string.IsNullOrWhiteSpace(firstName)
+            || int.TryParse(firstName, out int intFromName)
+            || double.TryParse(firstName, out double doubleFromName)
+        )
+        {
+            Console.WriteLine("Invalid first name.");
+            continue;
+        }
+
+        return firstName;
+    } while (true);
+}
+
+int AskUserForAge()
+{
+    do
+    {
+        var textAge = AskUserForInfo("What is your age: ");
+
+        if (
+            string.IsNullOrWhiteSpace(textAge)
+            || !int.TryParse(textAge, out int intFromName)
+            || intFromName < 1
+            || !double.TryParse(textAge, out double doubleFromName)
+        )
+        {
+            Console.WriteLine("Invalid age.");
+            continue;
+        }
+
+        return int.Parse(textAge);
+    } while (true);
+}
