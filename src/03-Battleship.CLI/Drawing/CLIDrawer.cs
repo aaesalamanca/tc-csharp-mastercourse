62e306d1a5d137d299e2e907ad1eae2ed9605413
aaesalamanca <aaesalamanca@gmail.com>
2025-03-18 15:19:34 +0100

    feat: Show welcome app info

---
+++ src/03-Battleship.CLI/Drawing/CLIDrawer.cs
@@ -0,0 +1,12 @@
+namespace _03_Battleship.CLI.Drawing;
+
+public static class CLIDrawer
+{
+    public static void ShowWelcome()
+    {
+        Console.WriteLine("###############################");
+        Console.WriteLine("####### Battleship Game #######");
+        Console.WriteLine("###############################");
+        Console.WriteLine();
+    }
+}
